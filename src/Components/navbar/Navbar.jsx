3fb9a1ce174ba3637fc816fe6d8ac0c1500425b4
AkshAy-K125 <akshay.raviraj@gmail.com>
2024-06-12 11:56:22 +0300

    cv update

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [sidePanel, setSidePanel] = useState(false)
 
   const resumeDownload = () => {
-    window.open('https://drive.google.com/file/d/1QLzp7rFL_cVMGrPd06gynuB0oQS7y8et/view?usp=sharing')
+    window.open('https://drive.google.com/file/d/1YEfQ8PCQOtdDPI1SO0cI0Mcb_M-oBV_d/view?usp=sharing')
   }
 
   return (
