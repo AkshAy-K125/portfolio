78b5e55246bb0e38d38874296859537c4b06cefe
akshay.kumar18 <akshay.kumar18@flipkart.com>
2025-09-19 02:23:03 +0530

    job order factory toggle price added

--- src/Components/quotations/NisrQuote818/NisrQuote.jsx
+++ src/Components/quotations/NisrQuote818/NisrQuote.jsx
@@ -56,6 +56,11 @@ const NisrQuote = () => {
                   <td>5</td>
                 </tr>
                 <tr>
+                  <td>Job completion status with Note</td>
+                  <td>Creation of a new widgetto manage and track job completion status with Note field</td>
+                  <td>2</td>
+                </tr>
+                <tr>
                   <td>Tax Toggle</td>
                   <td>Implementation of a toggle function to switch tax calculations on and off for LPOs.</td>
                   <td>1</td>
@@ -64,7 +69,7 @@ const NisrQuote = () => {
               <tfoot>
                 <tr>
                   <th colSpan="2" className="total-label">Total Estimated Effort</th>
-                  <td><strong>7 Days</strong></td>
+                  <td><strong>9 Days</strong></td>
                 </tr>
               </tfoot>
             </table>
@@ -95,7 +100,7 @@ const NisrQuote = () => {
         <div className="section">
           <h2 className="section-heading">Terms & Conditions</h2>
           <ul className="terms-list">
-            <li><strong>1. Payment Schedule:</strong> This is a fixed-fee project based on the total estimated effort. The total cost is <strong>119 BHD</strong>. An advance payment of <strong>40% (47.60 BHD)</strong> is required to initiate the project, with the remaining <strong>60% (71.40 BHD)</strong> due upon project completion.</li>
+            <li><strong>1. Payment Schedule:</strong> This is a fixed-fee project based on the total estimated effort. The total cost is <strong>153 BHD</strong>. An advance payment of <strong>40% (61.20 BHD)</strong> is required to initiate the project, with the remaining <strong>60% (91.80 BHD)</strong> due upon project completion.</li>
             <li><strong>2. Late Payments:</strong> Delayed payments may impact the project timeline. Future projects with a history of late payments will require a 100% advance payment.</li>
             <li><strong>3. Project Ownership & Confidentiality:</strong> Upon final payment, the client will have full ownership of the final design and code. We agree to a mutual, non-disclosure agreement (NDA) to ensure all shared information remains private and confidential.</li>
             <li><strong>4. Cancellation Policy:</strong> If the project is canceled by the client after work has begun, the initial 40% advance payment is non-refundable.</li>
